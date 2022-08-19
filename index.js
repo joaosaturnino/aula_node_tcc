dc17881d6fc0d70cbd37e444054f313f64acc1af
joaosaturnino <joao.saturnino@etec.sp.gov.br>
2022-08-19 19:45:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 // const port = process.env.Port || 3333;
 
-const porta = 3333;
+const porta = 3333
 
 // define a porta do servidor - definir de acordo com a oferecida pelo serviço de hospedagem
 app.listen(porta, () => {
