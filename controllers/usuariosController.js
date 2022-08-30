94e522f735fb4ee38d0bfeb40d9a7b2be13b316f
joaosaturnino <joao.saturnino@etec.sp.gov.br>
2022-08-30 19:18:02 -0300

    Update usuariosController.js

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -16,9 +16,9 @@ module.exports = {
     },
     async create(request, response) {
         try {
-            const {usuNome, usu_Email, usu_Senha, usu_Tipo, usu_Documento, usu_Moderacao } = request.body;
+            const {usuNome, usuEmail, usuSenha, usuTipo, usuDocumento, usuModeracao } = request.body;
 
-            const sql = 'INSERT INTO usuarios (usuNome, usuEmail, usuSenha, usuTipo, usuDocumento, usuModeracao) VALUES (?, ?, ?, ?, ?, ?)';
+            const sql = 'INSERT INTO USUARIOS (usuNome, usuEmail, usuSenha, usuTipo, usuDocumento, usuModeracao) VALUES (?, ?, ?, ?, ?, ?);';
             const values = [usuNome, usuEmail, usuSenha, usuTipo, usuDocumento, usuModeracao];
             const confirmacao = await db.query(sql, values);
             return response.status(200).json({confirma: 'Sucesso', message: usuId});
