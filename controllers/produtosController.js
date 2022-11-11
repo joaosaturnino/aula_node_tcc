a5b3e613ce2d74670bb52394cb89551dd03ba675
Hiago Oliveira <hiagokaua13@gmail.com>
2022-11-10 22:48:27 -0300

    Comite sem titulo tbm

--- controllers/produtosController.js
+++ controllers/produtosController.js
@@ -9,7 +9,7 @@ function geraUrl(e) {
         proNome: e.proNome,
         cat_Id: e.cat_Id,
         est_Id: e.est_Id,
-        proImagem: 'http://10.67.23.228:3333/public/upload/produtos/' + e.proImagem,
+        proImagem: 'http://10.67.23.166:3333/public/upload/produtos/' + e.proImagem,
         proAtualizacao: e.proAtualizacao,
         tamPreco: e.tamPreco,
         tamPrecoPromo: e.tamPrecoPromo,
@@ -104,7 +104,7 @@ module.exports = {
             const valuesCount = [proNomeProd, cat_Id, tamPrato, est_Id, tamPromo];
             const n_prod = await db.query(sqlCount, valuesCount);
 
-            const sql = ('SELECT proId, proNome, cat_Id, est_Id, proImagem, proAtualizacao, tamPreco, tamPrecoPromo, tamPrato, proDescricao FROM produtos Inner join Categorias cat ON cat_Id = cat.catId INNER JOIN Estabelecimentos est ON est_Id = est.estId INNER JOIN Tamanhos tm ON proId = tm.pro_Id  Where proNome LIKE ? AND cat_Id LIKE ? AND tamPrato LIKE ? AND est_Id LIKE ? AND tamPromo LIKE ? LIMIT ?, ?;');
+            const sql = ('SELECT proId, proNome, cat_Id, est_Id, proImagem, proAtualizacao, tamPreco, tamPrecoPromo, tamPrato, proDescricao FROM produtos Inner join Categorias cat ON cat_Id = cat.catId INNER JOIN Estabelecimentos est ON est_Id = est.estId INNER JOIN Tamanhos tm ON proId = tm.pro_Id  Where proNome LIKE ? AND cat_Id LIKE ? AND tamPrato LIKE ? AND est_Id LIKE ? AND tamPromo LIKE ? ORDER BY tamPreco LIMIT ?, ?;');
 
             const values = [proNomeProd, cat_Id, tamPrato, est_Id, tamPromo, parseInt(inicio), parseInt(limit)];
             const produtos = await db.query(sql, values);
