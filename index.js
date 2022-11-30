5d49de76773ad2e676b788e7fb8a0f3b5e946826
Sabongi <93168272+Sabongi@users.noreply.github.com>
2022-11-30 19:28:46 -0300

    Comentarios e coisas inuteis retiradas

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //const { request, response } = require('express');
 const express = require('express');
 const cors = require('cors');
-
 const router = require('./routes/routes');
 
 const app = express();
